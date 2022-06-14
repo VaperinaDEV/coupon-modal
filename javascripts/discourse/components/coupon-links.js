23b9facf863426a236d6c2c28bcb1b4d277b3e99
Don <71207900+VaperinaDEV@users.noreply.github.com>
2022-06-14 10:52:03 +0200

    Create coupon-links.js

---
+++ javascripts/discourse/components/coupon-links.js
@@ -0,0 +1,9 @@
+import Component from "@ember/component";
+import { computed } from "@ember/object";
+
+export default Component.extend({
+  
+  couponLinks: computed(function () {
+  return JSON.parse(settings.coupon_links);
+  }),
+});
