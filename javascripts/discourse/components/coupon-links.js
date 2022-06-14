f5adf7497057874f0d014cc9f33fae41f01c04df
Don <71207900+VaperinaDEV@users.noreply.github.com>
2022-06-14 10:53:56 +0200

    Update coupon-links.js

--- javascripts/discourse/components/coupon-links.js
+++ javascripts/discourse/components/coupon-links.js
@@ -1,7 +1,9 @@
 import Component from "@ember/component";
 import { computed } from "@ember/object";
+import { inject as service } from "@ember/service";
 
 export default Component.extend({
+  router: service(),
   
   couponLinks: computed(function () {
   return JSON.parse(settings.coupon_links);
