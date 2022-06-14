ce7741e96deba711be68c47a3a7528730b453708
Don <71207900+VaperinaDEV@users.noreply.github.com>
2022-06-14 12:01:31 +0200

    Update coupon-modal.js

--- javascripts/discourse/initializers/coupon-modal.js
+++ javascripts/discourse/initializers/coupon-modal.js
@@ -6,6 +6,8 @@ import { computed } from "@ember/object";
 let icon = iconNode('ticket-alt');
 
 export default {
+  name: "coupon-modal",
+  
   couponLinks: computed(function () {
     return JSON.parse(settings.coupon_links);
   }),
