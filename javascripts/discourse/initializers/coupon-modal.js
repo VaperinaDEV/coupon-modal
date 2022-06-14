a82ba3410fe9cd19a117ce4ce5549f6e8924a8e0
Don <71207900+VaperinaDEV@users.noreply.github.com>
2022-06-14 15:21:33 +0200

    Update coupon-modal.js

--- javascripts/discourse/initializers/coupon-modal.js
+++ javascripts/discourse/initializers/coupon-modal.js
@@ -1,16 +1,11 @@
 import { withPluginApi } from "discourse/lib/plugin-api";
 import showModal from "discourse/lib/show-modal";
 import { iconNode } from "discourse-common/lib/icon-library";
-import { computed } from "@ember/object";
 
 let icon = iconNode('ticket-alt');
 
 export default {
   name: "coupon-modal",
-  
-  couponLinks: computed(function () {
-    return JSON.parse(settings.coupon_links);
-  }),
   
   initialize() {
     withPluginApi("0.8.31", api => {
