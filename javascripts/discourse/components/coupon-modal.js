f97773f9694553189870112d8c915ec7b91b3ed5
Don <71207900+VaperinaDEV@users.noreply.github.com>
2022-06-14 15:23:04 +0200

    Create coupon-modal.js

---
+++ javascripts/discourse/components/coupon-modal.js
@@ -0,0 +1,8 @@
+import Component from "@ember/component";
+import { computed } from "@ember/object";
+export default Component.extend({
+
+  couponLinks: computed(function () {
+    return JSON.parse(settings.coupon_links);
+  }),
+});
