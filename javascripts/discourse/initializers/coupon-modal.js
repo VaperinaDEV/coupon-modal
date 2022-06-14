af2d8d0ceef467dcaf4c6aaa802f79e3b3ff498c
Don <71207900+VaperinaDEV@users.noreply.github.com>
2022-06-14 08:16:01 +0200

    Add files via upload

---
+++ javascripts/discourse/initializers/coupon-modal.js
@@ -0,0 +1,27 @@
+import { withPluginApi } from "discourse/lib/plugin-api";
+import showModal from "discourse/lib/show-modal";
+import { iconNode } from "discourse-common/lib/icon-library";
+
+let icon = iconNode('ticket-alt');
+
+export default {
+  name: "coupon-modal",
+
+  initialize(container) {
+    withPluginApi("0.8.31", api => {
+      if (api.getCurrentUser()) {
+        api.createWidget("modal-button", {
+          tagName: "a.vp-link.vp-modal.btn-default.btn.no-text.btn-icon",
+
+          html() {
+            return icon;
+          },
+
+          click() {
+            showModal("customModal");
+          }
+        });
+      }
+    });
+  }
+};
